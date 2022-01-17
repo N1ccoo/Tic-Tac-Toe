d5d4037042231f46df0bb4b6fe0f3868419aa7b7
Nicco <niccolino707@yahoo.com>
2022-01-17 20:52:39 +0800

    BUG FIXES! Added the game flow for ai mode.

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ const gameSettings = (() => {
     let formArea = document.getElementById('popup-form')
     let submitBtn = document.getElementById('form-submit-button')
 
-    settingsBtn.addEventListener('click',openForm)
+    settingsBtn.addEventListener('click', openForm)
 
     function closeForm(e) {
         let path = e.composedPath();
 
-        const withinBoundaries = path.includes(settingsBtn) || path.includes(formArea) 
+        const withinBoundaries = path.includes(settingsBtn) || path.includes(formArea)
 
-        if(!(withinBoundaries)) {
+        if (!(withinBoundaries)) {
             formArea.classList.add('close')
             formArea.classList.remove('open')
         }
@@ -28,31 +28,43 @@ const gameSettings = (() => {
 
 const Game = (() => {
 
-    const User = (name,mark) => {
+    const User = (name, mark) => {
         state = {
             name,
             mark,
             turn: false
         }
-    
-        return Object.assign({},userMethods(state))
-    } 
-    
+
+        return Object.assign({}, userMethods(state))
+    }
+
     const userMethods = (state) => ({
-        setName: (newName) => {return state.name = newName},
-        setMark: (newMark) => {return state.mark = newMark},
-        setTurn: (newTurn) => {return state.turn = newTurn},
-        getName: () => {return state.name},
-        getMark: () => {return state.mark},
-        getTurn: () => {return state.turn},
-        toggleTurn: () => {return state.turn = (!state.turn)}
+        setName: (newName) => {
+            return state.name = newName
+        },
+        setMark: (newMark) => {
+            return state.mark = newMark
+        },
+        setTurn: (newTurn) => {
+            return state.turn = newTurn
+        },
+        getName: () => {
+            return state.name
+        },
+        getMark: () => {
+            return state.mark
+        },
+        getTurn: () => {
+            return state.turn
+        },
+        toggleTurn: () => {
+            return state.turn = (!state.turn)
+        }
     })
 
-
-    let userOne = User('Player 1','X')
-    let userTwo = User('Player 2','O')
-    let userArray = [userOne,userTwo]
-  
+    let userOne = User('Player 1', 'X')
+    let userTwo = User('Player 2', 'O')
+    let userArray = [userOne, userTwo]
 
 
     let settingsFormInput = document.getElementById('settings-form')
@@ -64,7 +76,16 @@ const Game = (() => {
     let userInputSubmit = document.getElementById('form-submit-button')
 
 
-    let winConditions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]  
+    let winConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
     let twoPlayerBtn = document.getElementById('two-player-mode')
     let computerBtn = document.getElementById('computer-mode')
     let gameText = document.getElementById('footer')
@@ -72,25 +93,20 @@ const Game = (() => {
     let gameTurns = []
     let didWinHappen = false
 
-    twoPlayerBtn.addEventListener('click',startTwoPlayerGame)
-    settingsFormInput.addEventListener('submit',setUserInfo)
-    userTurnInput.addEventListener('click',swapFirstTurn)
+    twoPlayerBtn.addEventListener('click', startTwoPlayerGame)
+    settingsFormInput.addEventListener('submit', setUserInfo)
+    userTurnInput.addEventListener('click', swapFirstTurn)
 
-    function squareClick(e) {
-        if (e.target.textContent !== userOne.getMark() && e.target.textContent !== userTwo.getMark()) {
-            playerTurn = userArray.filter(getPlayerTurn)
-            e.target.textContent = playerTurn[0].getMark()
-            updateUserArrayTurn()
-            checkWin()
+
+    const getPlayerTurn = (item) => {
+        if (item.getTurn() === true) {
+            return item
         }
     }
 
-    const getPlayerTurn = (item) => {
-        if (item.getTurn() === true) {return item}
-    }
-
     const updateUserArrayTurn = () => {
-        userArray.forEach(item => {item.toggleTurn()})
+        userOne.toggleTurn()
+        userTwo.toggleTurn()
     }
 
     const checkWinLogic = (winCon) => {
@@ -102,40 +118,55 @@ const Game = (() => {
         let markTwo = userTwo.getMark();
         let userOneWin = squareTextOne === markOne && squareTextTwo === markOne && squareTextThree === markOne;
         let userTwoWin = squareTextOne === markTwo && squareTextTwo === markTwo && squareTextThree === markTwo;
-       
-        
+
+
         if (userOneWin) {
             gameText.textContent = `${userOne.getName()} has won in ${gameTurns.length} turns!`
             didWinHappen = true
             removeEventSquares()
+            removeComputerEventSquares()
 
         } else if (userTwoWin) {
             gameText.textContent = `${userTwo.getName()} has won in ${gameTurns.length} turns!`
             didWinHappen = true
             removeEventSquares()
-        }  
+            removeComputerEventSquares()
+        }
     }
 
     function checkWin() {
         gameTurns.push(null)
-        winConditions.forEach(winCon => {checkWinLogic(winCon)})
+        winConditions.forEach(winCon => {
+            checkWinLogic(winCon)
+        })
         if (didWinHappen === false && gameTurns.length === 9) {
             gameText.textContent = "DRAW"
         }
-    } 
+    }
 
     function removeEventSquares() {
-        gameSquare.forEach((item) => {item.removeEventListener('click',squareClick)})
+        gameSquare.forEach((item) => {
+            item.removeEventListener('click', squareClick)
+        })
     }
 
     function addEventSquares() {
-        gameSquare.forEach((item) => {item.addEventListener('click',squareClick)})
+        gameSquare.forEach((item) => {
+            item.addEventListener('click', squareClick)
+        })
     }
-    
+
+    function removeComputerEventSquares() {
+        gameSquare.forEach((item) => {
+            item.removeEventListener('click', computerSquareClick)
+            item.removeEventListener('click', skipTurn)
+        })
+    }
+
     function startTwoPlayerGame() {
         resetGame()
+        addEventSquares()
         gameSquare.forEach((item) => {
-            item.addEventListener('click',squareClick);
             item.textContent = '';
             item.classList.remove('board-highlight');
         })
@@ -144,17 +175,19 @@ const Game = (() => {
     }
 
     function resetGame() {
+        removeComputerEventSquares()
         removeEventSquares()
         gameTurns = []
         didWinHappen = false
-        addEventSquares()
+        userOne.setTurn(false)
+        userTwo.setTurn(false)
     }
 
     function setUserInfo(e) {
         e.preventDefault()
-        userOne.setName(userOneNameInput.value)  
-        userTwo.setName(userTwoNameInput.value) 
-        userOne.setMark(userOneMarkInput.value) 
+        userOne.setName(userOneNameInput.value)
+        userTwo.setName(userTwoNameInput.value)
+        userOne.setMark(userOneMarkInput.value)
         userTwo.setMark(userTwoMarkInput.value)
         setUserTurns(userTurnInput.value)
         userOneNameInput.value = ''
@@ -167,29 +200,98 @@ const Game = (() => {
         if (choice === 'userOneFirst') {
             userOne.setTurn(true)
             userTwo.setTurn(false)
+            return 'user one first'
         } else if (choice === 'userTwoFirst') {
             userOne.setTurn(false)
             userTwo.setTurn(true)
+            return 'user two first'
         }
     }
 
     function swapFirstTurn() {
-        if(userTurnInput.value === 'userOneFirst') {
-            userTurnInput.setAttribute('value','userTwoFirst')
-            userTurnInput.textContent = 'Player 2 First'  
+        if (userTurnInput.value === 'userOneFirst') {
+            userTurnInput.setAttribute('value', 'userTwoFirst')
+            userTurnInput.textContent = 'Player 2 First'
         } else if (userTurnInput.value === 'userTwoFirst') {
-            userTurnInput.setAttribute('value','userOneFirst')
+            userTurnInput.setAttribute('value', 'userOneFirst')
             userTurnInput.textContent = 'Player 1 First'
         }
-      
+
     }
 
+    
 
+    function squareClick(e) {
+        if (e.target.textContent !== userOne.getMark() && e.target.textContent !== userTwo.getMark()) {
+            playerTurn = userArray.filter(getPlayerTurn)
+            e.target.textContent = playerTurn[0].getMark()
+            updateUserArrayTurn()
+            checkWin()
+        }
+    }
+
+        computerBtn.addEventListener('click', startComputerGame)
+
+        function startComputerGame() {
+            resetGame()
+            gameSquare.forEach((item) => {
+                item.addEventListener('click', computerSquareClick);
+                item.addEventListener('click', skipTurn);
+                item.textContent = '';
+                item.classList.remove('board-highlight');
+            })
+            gameText.textContent = ''
+            setUserTurns('userOneFirst')
+
+            if(userTurnInput.value == 'userTwoFirst') {
+                gameSquare[4].textContent = userTwo.getMark()
+                gameSquare[4].classList.add('board-highlight')
+            }
+
+        }
+
+        function computerSquareClick(e) {
+            let bestScore = -Infinity;
+            let bestMove = ''
+
+            if (e.target.textContent !== userOne.getMark() && e.target.textContent !== userTwo.getMark()) {
+                playerTurn = userArray.filter(getPlayerTurn)
+                e.target.textContent = playerTurn[0].getMark()
+                updateUserArrayTurn()
+                checkWin()
+                for (i = 0; i < gameSquare.length; i++) {
+                    if (gameSquare[i].textContent == '') {
+                        gameSquare[i].textContent = userTwo.getMark();
+                        let score = minimax(gameSquare);
+                        gameSquare[i].textContent = '';
+                        console.log(gameSquare[i])
+                        if (score > bestScore) {
+                            bestScore = score;
+                            bestMove = {i};
+                        }
+                    }
+                }
+                console.log('nicco')
+            }
+            console.log(i)
+            gameSquare[bestMove.i].textContent = userTwo.getMark()
+            gameSquare[bestMove.i].classList.add('board-highlight')
+            checkWin()
+        }
+    
+        function skipTurn() {
+            setUserTurns('userOneFirst')
+        }
+        
+        function minimax(board) {
+            return 1;
+        }
+
+    
 
     return Object.assign({})
 
 })()
-
 
 const Style = (() => {
 
@@ -197,13 +299,13 @@ const Style = (() => {
     let gameSquare = Array.from(document.getElementsByClassName('game-square'))
 
     navButtons.forEach((item) => {
-        item.addEventListener('mouseenter',hoverSquareBtn);
-        item.addEventListener('mouseleave',removeHoverSquareBtn);
+        item.addEventListener('mouseenter', hoverSquareBtn);
+        item.addEventListener('mouseleave', removeHoverSquareBtn);
     })
 
     gameSquare.forEach((item) => {
-        item.addEventListener('mouseenter',hoverSquare);
-        item.addEventListener('mouseleave',removeHoverSquare);
+        item.addEventListener('mouseenter', hoverSquare);
+        item.addEventListener('mouseleave', removeHoverSquare);
         item.addEventListener('click', highlightSquare)
     })
 
